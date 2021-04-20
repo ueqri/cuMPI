29eca1aae85b8149d92bed9963adbb23fe6930f6
ueqri <ueqri@outlook.com>
2021-04-20 17:03:40 +0800

    finished Allreduce & SendRecv

---
+++ src/cuMPI.cu
@@ -0,0 +1,202 @@
+#include "cuMPI_runtime.h"
+
+int myRank, nRanks, localRank = 0;
+ncclUniqueId id;
+cuMPI_Comm comm;
+cudaStream_t defaultStream;
+uint64_t hostHashs[10];
+char hostname[1024];
+
+static uint64_t getHostHash(const char *string) {
+  // based on DJB2, result = result * 33 + char
+  uint64_t result = 5381;
+  for (int c = 0; string[c] != '\0'; c++) {
+    result = ((result << 5) + result) + string[c];
+  }
+  return result;
+}
+
+static void getHostName(char *hostname, int maxlen) {
+  gethostname(hostname, maxlen);
+  for (int i = 0; i < maxlen; i++) {
+    if (hostname[i] == '.') {
+      hostname[i] = '\0';
+      return;
+    }
+  }
+}
+
+
+
+int cuMPI_Comm_size(MPI_Comm comm, int *size){return 0;}
+int cuMPI_Comm_rank(MPI_Comm comm, int *rank){return 0;}
+
+
+
+int cuMPI_AllocateOneGPUPerProcess() {
+  // TODO
+  cuMPI_Init(NULL, NULL);
+  return 0;
+}
+
+
+
+int testSendRecv() {
+  cuMPI_Init(NULL, NULL);
+  
+  int count = 50;
+  float *h_send = (float *)malloc(count * sizeof(float)),
+        *h_recv = (float *)malloc(count * sizeof(float));
+  for (int i = 0; i < count; ++i) {
+    h_send[i] = 2 * i + localRank;
+  }
+
+  float *d_send = NULL, *d_recv = NULL;
+  CUDA_CHECK(cudaMalloc(&d_send, count * sizeof(float)));
+  CUDA_CHECK(cudaMalloc(&d_recv, count * sizeof(float)));
+  
+  CUDA_CHECK(cudaMemcpy(d_send, h_send, count * sizeof(float), cudaMemcpyHostToDevice));
+
+  cuMPI_Status status;
+  int peer = 1 - localRank;
+  cuMPI_Sendrecv(d_send, count, cuMPI_FLOAT, peer, 0, d_recv, count, cuMPI_FLOAT, localRank, 0, comm, &status);
+
+  CUDA_CHECK(cudaMemcpy(h_recv, d_recv, count * sizeof(float), cudaMemcpyDeviceToHost));
+  
+  printf("[%d]:\n", localRank);
+  for (int i = 0; i < count; ++i) {
+    printf("%d->%d ", (int)h_send[i], (int)h_recv[i]);
+  }
+
+  CUDA_CHECK(cudaFree(d_send));
+  CUDA_CHECK(cudaFree(d_recv));
+  free(h_send);
+  free(h_recv);
+  cuMPI_Finalize();
+  return 0;
+}
+int testAllReduce() {
+  cuMPI_Init(NULL, NULL);
+  
+  int count = 50;
+  float *h_send = (float *)malloc(count * sizeof(float)),
+        *h_recv = (float *)malloc(count * sizeof(float));
+  for (int i = 0; i < count; ++i) {
+    h_send[i] = 2 * i + localRank;
+  }
+
+  float *d_send = NULL, *d_recv = NULL;
+  CUDA_CHECK(cudaMalloc(&d_send, count * sizeof(float)));
+  CUDA_CHECK(cudaMalloc(&d_recv, count * sizeof(float)));
+  
+  CUDA_CHECK(cudaMemcpy(d_send, h_send, count * sizeof(float), cudaMemcpyHostToDevice));
+
+  cuMPI_Allreduce(d_send, d_recv, count, cuMPI_FLOAT, cuMPI_SUM, comm);
+
+  CUDA_CHECK(cudaMemcpy(h_recv, d_recv, count * sizeof(float), cudaMemcpyDeviceToHost));
+  
+  printf("[%d]:\n", localRank);
+  for (int i = 0; i < count; ++i) {
+    printf("%d->%d ", (int)h_send[i], (int)h_recv[i]);
+  }
+
+  CUDA_CHECK(cudaFree(d_send));
+  CUDA_CHECK(cudaFree(d_recv));
+  free(h_send);
+  free(h_recv);
+  cuMPI_Finalize();
+  return 0;
+}
+
+int cuMPI_Initialized(int *flag) { 
+  return MPI_Initialized(flag);
+}
+
+int cuMPI_Init(int *argc, char ***argv) {
+
+  // initializing MPI
+  MPI_CHECK(MPI_Init(argc, argv));
+  MPI_CHECK(MPI_Comm_rank(MPI_COMM_WORLD, &myRank));
+  MPI_CHECK(MPI_Comm_size(MPI_COMM_WORLD, &nRanks));
+
+  // calculating localRank based on hostname which is used in selecting a GPU
+  getHostName(hostname, 1024);
+  hostHashs[myRank] = getHostHash(hostname);
+  MPI_CHECK(MPI_Allgather(MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, hostHashs,
+                          sizeof(uint64_t), MPI_BYTE, MPI_COMM_WORLD));
+  for (int p = 0; p < nRanks; p++) {
+    if (p == myRank) {
+      break;
+    }
+    if (hostHashs[p] == hostHashs[myRank]) {
+      localRank++;
+    }
+  }
+
+  // get NCCL unique ID at rank 0 and broadcast it to all others
+  if (myRank == 0) {
+    ncclGetUniqueId(&id);
+  }
+  MPI_CHECK(MPI_Bcast((void *)&id, sizeof(id), MPI_BYTE, 0, MPI_COMM_WORLD));
+
+  // picking a GPU based on localRank, allocate device buffers
+  printf("Picking Device: %d for MPI Rank: %d/%d\n", localRank, myRank, nRanks);
+  CUDA_CHECK(cudaSetDevice(localRank));
+  CUDA_CHECK(cudaStreamCreate(&defaultStream));
+
+  printf("[%d]: from stream %p\n", localRank, (void*)defaultStream);
+
+  // initializing NCCL
+  NCCL_CHECK(ncclCommInitRank(&comm, nRanks, id, myRank));
+  printf("Initiated NCCL for MPI Rank: %d/%d\n", myRank, nRanks);
+
+  return 0;
+}
+
+int cuMPI_Finalize(){
+  NCCL_CHECK(ncclCommDestroy(comm));  // finalizing NCCL
+  MPI_CHECK(MPI_Finalize());          // finalizing MPI
+  printf("[MPI Rank %d] Success.\n", myRank);
+  return 0;
+}
+
+int cuMPI_Allreduce(const void *sendbuf, void *recvbuf, int count,
+  cuMPI_Datatype datatype, cuMPI_Op op, cuMPI_Comm comm){
+  #if CUMPI_DEBUG > 1
+  printf("AllReduce Communicating...\n");
+  #endif
+  NCCL_CHECK(ncclAllReduce((const void *)sendbuf, (void *)recvbuf, count,
+                            datatype, op, comm, defaultStream));
+
+  // completing NCCL operation by synchronizing on the CUDA stream
+  CUDA_CHECK(cudaStreamSynchronize(defaultStream));
+  return 0;
+}
+
+int cuMPI_Sendrecv(const void *sendbuf, int sendcount, cuMPI_Datatype sendtype,
+  int dest, int sendtag,
+  void *recvbuf, int recvcount, cuMPI_Datatype recvtype,
+  int source, int recvtag,
+  cuMPI_Comm comm, cuMPI_Status *status) {
+  assert(sendtag == recvtag);
+  //(void*)(status), (void)(source); // variable not use
+
+  #if CUMPI_DEBUG > 1
+  printf("Send&Receive Communicating...\n");
+  #endif
+  // peer rank id is `dest`
+  ncclGroupStart();
+  ncclSend(sendbuf, sendcount, sendtype, dest, comm, defaultStream);
+  ncclRecv(recvbuf, recvcount, recvtype, dest, comm, defaultStream);
+  ncclGroupEnd();
+
+  return 0;
+}
+
+int cuMPI_Bcast( void *buffer, int count, cuMPI_Datatype datatype, int root, 
+  cuMPI_Comm comm ) {
+  
+}
+
+// int MPI_Bcast
+// int barrier
---
+++ src/cuMPI_runtime.h
@@ -0,0 +1,117 @@
+#ifndef _CUMPI_RUNTIME_H_
+#define _CUMPI_RUNTIME_H_
+
+#include "cuda_runtime.h"
+#include "nccl.h"
+#include "mpi.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <assert.h>
+
+#define MPI_CHECK(cmd) do {                      \
+  int e = cmd;                                   \
+  if (e != MPI_SUCCESS) {                        \
+    printf("Failed: MPI error %s:%d '%d'\n",     \
+     __FILE__,__LINE__, e);                      \
+    exit(EXIT_FAILURE);                          \
+  }                                              \
+} while(0)
+
+#define CUDA_CHECK(cmd) do {                     \
+  cudaError_t e = cmd;                           \
+  if (e != cudaSuccess) {                        \
+    printf("Failed: CUDA error %s:%d '%s'\n",    \
+     __FILE__,__LINE__,cudaGetErrorString(e));   \
+    exit(EXIT_FAILURE);                          \
+  }                                              \
+} while(0)
+
+#define NCCL_CHECK(cmd) do {                     \
+  ncclResult_t r = cmd;                          \
+  if (r != ncclSuccess) {                        \
+    printf("Failed, NCCL error %s:%d '%s'\n",    \
+     __FILE__,__LINE__,ncclGetErrorString(r));   \
+    exit(EXIT_FAILURE);                          \
+  }                                              \
+} while(0)
+
+typedef ncclComm_t cuMPI_Comm;
+typedef ncclDataType_t cuMPI_Datatype;
+typedef ncclRedOp_t cuMPI_Op;
+typedef MPI_Status cuMPI_Status;
+
+#define cuMPI_SUM ncclSum
+#define cuMPI_PROD ncclProd
+#define cuMPI_MIN ncclMin
+#define cuMPI_MAX ncclMax
+
+/**************
+ * Data Types *
+ **************/
+
+// Signed 8-bits integer
+#define cuMPI_INT8_T ncclInt8
+
+// Signed 8-bits integer
+#define cuMPI_CHAR ncclChar
+
+// Unsigned 8-bits integer
+#define cuMPI_UINT8_T ncclUint8
+
+// Signed 32-bits integer
+#define cuMPI_INT32_T ncclInt32
+
+// Signed 32-bits integer
+#define cuMPI_INT ncclInt
+
+// Unsigned 32-bits integer
+#define cuMPI_UINT32_T ncclUint32
+
+// Signed 64-bits integer
+#define cuMPI_INT64_T ncclInt64
+
+// Unsigned 64-bits integer
+#define cuMPI_UINT64_T ncclUint64
+
+// [UNSUPPORTED IN MPI]
+// 16-bits floating point number (half precision)
+// #define ncclFloat16
+
+// [UNSUPPORTED IN MPI]
+// 16-bits floating point number (half precision)
+// #define ncclHalf
+
+// [SAME WITH MPI_FLOAT]
+// 32-bits floating point number (single precision)
+// #define ncclFloat32
+
+// 32-bits floating point number (single precision)
+#define cuMPI_FLOAT ncclFloat
+
+// [SAME WITH MPI_DOUBLE]
+// 64-bits floating point number (double precision)
+// #define ncclFloat64
+
+// 64-bits floating point number (double precision)
+#define cuMPI_DOUBLE ncclDouble
+
+
+
+int cuMPI_Initialized( int *flag );
+int cuMPI_Init( int *argc, char ***argv );
+int cuMPI_Finalize();
+int cuMPI_Allreduce(const void *sendbuf, void *recvbuf, int count,
+  cuMPI_Datatype datatype, cuMPI_Op op, cuMPI_Comm comm);
+int cuMPI_Sendrecv(const void *sendbuf, int sendcount, cuMPI_Datatype sendtype,
+  int dest, int sendtag,
+  void *recvbuf, int recvcount, cuMPI_Datatype recvtype,
+  int source, int recvtag,
+  cuMPI_Comm comm, cuMPI_Status *status);
+
+
+
+int cuMPI_Comm_size( MPI_Comm comm, int *size );
+int cuMPI_Comm_rank( MPI_Comm comm, int *rank );
+
+#endif
