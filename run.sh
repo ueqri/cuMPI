4bb5d0b8e4ea8b6759a3a7bb6ffc955c15b82e23
ueqri <ueqri@outlook.com>
2021-04-20 19:28:56 +0800

    added compile & run script

---
+++ run.sh
@@ -0,0 +1,14 @@
+# assume using OpenMPI
+export OMPI_ALLOW_RUN_AS_ROOT=1
+export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
+
+if [ -d "build" ]; then
+  rm -rf build
+fi
+mkdir build
+cd build
+cmake -DNCCL_LIBRARY=/lib64/libnccl.so \
+      -DNCCL_INCLUDE_DIR=/usr/include/ \
+      ..
+make -j$(procs) && \
+mpirun -np 2 src/cuMPI-demo
